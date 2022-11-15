bef5c6e83ffce2daee9d3c887b6e62fff0a4623f
Everton Cordeiro <99029999+EvertonCordeiro1994@users.noreply.github.com>
2022-11-15 10:49:56 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,16 +3,21 @@ input = document.querySelector("#conta")
 botao = document.querySelector('#btn')
 display = document.querySelector("#mostrador")
 
+
+
+
 botao.addEventListener('click', res = () =>{ 
     calc = input.value.replace(",",".").replace("²",`**${Number(2)}`).replace("³",`**${Number(3)}`)
     if(calc != ""){
         display.innerHTML = `${calc} <br> tem como resultado <br>  ${eval(calc)}`
         input.value = ""
     }else{
-        display.innerHTML = `<h1 id="alerta">Digite algum número</h1>`
+        display.innerHTML = `<h1 style="color: red;">Digite algum número</h1>`
     }
 })
 
+
+input.addEventListener('focus',  () =>  display.innerHTML = `<h1> </h1>` )
 
 input.addEventListener("keyup", function (e){
     if (e.keyCode === 13){
